15e7bddad2c497f46d31747ebcc9f4d3bef3374b
bipinlongjam <bipinlongjam11@gmail.com>
2024-05-24 17:27:32 +0530

    add backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 const app = express();
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js"
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes);
 
 // app.get("/",(req, res) =>{
 //     //root route http://localhost:5000/
